7bdd7ce69eabedd1dfc4247259df931d82205a29
Zexing Zhang <89085679+ZacheryZhang@users.noreply.github.com>
2024-06-21 13:41:10 +0800

    add

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview';
-import { Dialog,Message,Loading } from 'element-ui';
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/less/index.less';
 import * as echarts from 'echarts';
@@ -16,9 +16,7 @@ Vue.prototype.$images = img
 Vue.config.productionTip = false;
 Vue.use(iView);
 Vue.use(utils)
-Vue.use(Dialog);
-Vue.use(Message);
-Vue.use(Loading);
+Vue.use(ElementUI);
 new Vue({
   router,
   render: h => h(App)
